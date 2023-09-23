032515ed365f9820b9c0ab7c34bda7369273652d
aiirds <144480214+aiirds@users.noreply.github.com>
2023-09-23 09:52:35 +0000

    lab2 task2 change revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+  // your code here
+  int len=1, n;
+  char temp;  
+  
+  for (int i = 0; str[i] != '\0'; i++){
+      len = i;
+      n = i;
+  }
+  
+   for(int i = 0; i <=(len/2); i++)
+  {
+    temp=str[i];
+    str[i]=str[n];
+    str[n]=temp;
+    n--;
+  }
 }
-
